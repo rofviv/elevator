f1ace88311abb28aa0d6c375be5e03ddb2b629f6
rofviv <royvillarroel94@gmail.com>
2025-06-20 10:06:47 -0400

    timezone -4

--- esp-outputs.php
+++ esp-outputs.php
@@ -73,7 +73,7 @@
         while ($row = $result2->fetch_assoc()) {
             $row_reading_time = $row["last_request"];
             // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
-            //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+            $row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 4 hours"));
 
             // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
             //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 7 hours"));
